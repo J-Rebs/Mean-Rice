cc6ef58d6b29f4a98256540065a4cb0256b24eea
Joe Rebagliati <84640075+J-Rebs@users.noreply.github.com>
2022-11-23 18:24:59 -0500

    updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,8 @@
   import "../app.css";
 </script>
 
-<main class="container mx-auto px-2, py-2">
-    <h1 class="text-xl font-bold ">Mean Rice</h1>
+<main>
+    <h1 class="text-xl font-bold "><a href="/">Mean Rice</a></h1>
     <slot  /> 
 </main>
 
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,53 @@
 <script>
+  // Style src: https://kopi.dev/tailwind/gradient-background-animation/
 </script>
 
-<h2>About</h2>
+<div
+  class="
+    min-h-screen
+    min-w-screen
+    bg-gradient-to-r
+    from-pink-500
+    via-red-500
+    to-yellow-500
+    background-animate
+  "
+>
+  <div class="opacity-80 bg-white hover:opacity-100 px-6 py-6">
+    <h2 class="text-lg">About</h2>
 
-<p>This is a demo client of type 1, built in Svelte-Kit</p>
+    <p class="text-base">
+      This is a demo client of type 1, built in <a
+        class="text-cyan-500 font-bold"
+        href="https://kit.svelte.dev/">SvelteKit</a
+      >
+      and
+      <a class="text-cyan-500 font-bold" href="https://tailwindcss.com/"
+        >Tailwind CSS</a
+      >. It is deployed using
+      <a class="text-cyan-500 font-bold" href="https://vercel.com/docs"
+        >Vercel</a
+      >.
+    </p>
+  </div>
+</div>
+
+<style>
+  .background-animate {
+    background-size: 400%;
+
+    -webkit-animation: AnimationName 3s ease infinite;
+    -moz-animation: AnimationName 3s ease infinite;
+    animation: AnimationName 3s ease infinite;
+  }
+
+  @keyframes AnimationName {
+    0%,
+    100% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+  }
+</style>
