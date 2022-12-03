0b2d29b785cdb52749fedbb36d6f0690977f483a
Joe Rebagliati <84640075+J-Rebs@users.noreply.github.com>
2022-12-03 02:46:12 -0500

    test

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -6,8 +6,6 @@
 
   onMount(async () => {
     let options = {
-      method: "POST",
-      url: "https://dev-j67sh1xxw6ozyqup.us.auth0.com/oauth/token",
       headers: { "content-type": "application/x-www-form-urlencoded" },
       data: new URLSearchParams({
         grant_type: "client_credentials",
@@ -15,10 +13,10 @@
         client_secret:
           "SlF6GWa5phb-jx3bNS_GcRTosVeYoSVvz3U5aAQlV-e7JZ6DkEzisdQlnEEmykwq",
         audience: "https://music-tonic.herokuapp.com/",
-      }),
+      })
     };
 
-    const res = await axios.request(options);
+    const res = await axios.post("https://dev-j67sh1xxw6ozyqup.us.auth0.com/oauth/token",options);
     token = res.data;
     console.log("login token", token);
   });
