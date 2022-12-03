997b810272888422293e7e3c76d0e4722d7f254c
Joe Rebagliati <84640075+J-Rebs@users.noreply.github.com>
2022-12-03 02:40:04 -0500

    updates

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -1,25 +1,29 @@
 <script>
-  import axios from "axios";
-  import { onMount } from "svelte";
+import axios from "axios";
+import { onMount } from 'svelte';
 
-  let token;
+let token;
 
-  onMount(async () => {
-    let options = {
-      method: "POST",
-      url: "https://dev-j67sh1xxw6ozyqup.us.auth0.com/oauth/token",
-      headers: { "content-type": "application/x-www-form-urlencoded" },
-      data: new URLSearchParams({
-        grant_type: "client_credentials",
-        client_id: "6eZqG8pgM7D87N3GjyIPt8SrB8AeDoBb",
-        client_secret:
-          "SlF6GWa5phb-jx3bNS_GcRTosVeYoSVvz3U5aAQlV-e7JZ6DkEzisdQlnEEmykwq",
-        audience: "https://music-tonic.herokuapp.com/",
-      }),
-    };
+onMount(async () => {
 
-    const res = await axios.request(options);
-    token = res.data;
-    console.log("login token", token);
-  });
+let options = {
+  method: 'POST',
+  url: 'https://dev-j67sh1xxw6ozyqup.us.auth0.com/oauth/token',
+  headers: {'content-type': 'application/x-www-form-urlencoded'},
+  data: new URLSearchParams({
+    grant_type: 'client_credentials',
+    client_id: 'yVgHRKrNqFXv44dUCnUiUVGyowqbSiwL',
+    client_secret: 'beb62xgkt8mZd0vIe9S0zmyG8lqElpSUzh2dZFxgDeXnaA5Z-wRXAzdML0I-CG2c',
+    audience: 'https://music-tonic.herokuapp.com/'
+  })
+};
+
+const res = await axios.request(options)
+token = res.data;
+console.log('login token', token);
+
+
+    
+	});
+
 </script>
