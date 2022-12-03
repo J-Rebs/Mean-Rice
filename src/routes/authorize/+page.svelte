1e8c73c85563a1bc3b91b94439de1eb131084310
Joe Rebagliati <84640075+J-Rebs@users.noreply.github.com>
2022-12-03 06:49:55 -0500

    update

--- src/routes/authorize/+page.svelte
+++ src/routes/authorize/+page.svelte
@@ -1,25 +1,37 @@
 <script>
   import axios from "axios";
   import { onMount } from "svelte";
-  let res;
+  import * as jquery from "jquery";
+  
+
+  let result;
+
+
   
   onMount(async () => {
-   /**
-    * 
-    * 
-    * 
-    */
-    var options = { method: 'POST',
-      url: 'https://dev-j67sh1xxw6ozyqup.us.auth0.com/oauth/token',
-      headers: { 'content-type': 'application/json' },
-      body: '{"client_id":"I8ilmR0YJ6OD1Vo2hTzRNHOpSJinmGUt","client_secret":"gol_AM1EaULwKFS_93pn53FrHdWLc5Oj10fCxv1VP_NIqPA6S0foHF2xr-AO3dlK","audience":"https://mean-rice.vercel.app/authorize","grant_type":"client_credentials"}' };
+    var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://dev-j67sh1xxw6ozyqup.us.auth0.com/oauth/token",
+  "method": "POST",
+  "headers": {
+    "content-type": "application/json"
+  },
+  "data": "{\"client_id\":\"I8ilmR0YJ6OD1Vo2hTzRNHOpSJinmGUt\",\"client_secret\":\"gol_AM1EaULwKFS_93pn53FrHdWLc5Oj10fCxv1VP_NIqPA6S0foHF2xr-AO3dlK\",\"audience\":\"https://mean-rice.vercel.app/authorize\",\"grant_type\":\"client_credentials\"}"
+}
+
+jquery.ajax(settings).done(function (response) {
+  console.log(response);
+});
 
 
-    console.log('authentication commenced...');
-    let res = await axios.request(options);
-    console.log('authentication terminated...');
 
 
 
   });
 </script>
+
+<svelte:head>
+	<script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+</svelte:head>
+
